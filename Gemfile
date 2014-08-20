0b353174f309807f321d9103bce39b1abb72f9ad
Masashi AKISUE <masashi.akisue@optim.co.jp>
2014-08-20 16:59:34 +0900

    please do reviewを追加

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'http://rubygems.org/'
 
 gem 'cinch'
 gem 'configatron', '2.10.0'
+gem 'nokogiri'
--- mikuji_bot.rb
+++ mikuji_bot.rb
@@ -1,6 +1,7 @@
 require 'cinch'
 require 'json'
 require 'configatron'
+require 'nokogiri'
 
 class Mikuji
   include Cinch::Plugin
@@ -34,13 +35,44 @@ class Qiita
   end
 end
 
+class PleaseDoReview
+  include Cinch::Plugin
+
+  timer 60, method: :check_report
+
+  def check_report
+    h = Time.now.hour
+    m = Time.now.min
+
+    print_message if configatron.defaults.please_do_review.hour == h && configatron.defaults.please_do_review.min == m
+  end
+
+  def print_message
+    report_command = "curl --user #{configatron.defaults.please_do_review.user}:#{configatron.defaults.please_do_review.password} -L #{configatron.defaults.please_do_review.trac_report_url}"
+    html = `#{report_command}`
+    report_num = Nokogiri::HTML.parse(html).css(".numrows").first.children.text.match(/\d+/)[0]
+
+
+    Channel(configatron.defaults.please_do_review.irc_channel).send(configatron.defaults.please_do_review.frame);
+    Channel(configatron.defaults.please_do_review.irc_channel).send(configatron.defaults.please_do_review.message);
+    Channel(configatron.defaults.please_do_review.irc_channel).send("#{configatron.defaults.please_do_review.count_message_prefix}#{report_num}#{configatron.defaults.please_do_review.count_message_suffix}");
+
+    Channel(configatron.defaults.please_do_review.irc_channel).send(" ");
+    Channel(configatron.defaults.please_do_review.irc_channel).send(configatron.defaults.please_do_review.trac_report_url);
+
+    Channel(configatron.defaults.please_do_review.irc_channel).send(configatron.defaults.please_do_review.frame);
+
+    sleep(10)
+  end
+end
+
 bot = Cinch::Bot.new do
   configure do |c|
     configatron.configure_from_yaml("config/config.yml")
     c.server   = configatron.defaults.irc.server_address.to_s
     c.channels = configatron.defaults.irc.joining_channels
     c.nick     = configatron.defaults.bot.nick
-    c.plugins.plugins = [Mikuji, Qiita]
+    c.plugins.plugins = [Mikuji, Qiita, PleaseDoReview]
   end
 end
 
